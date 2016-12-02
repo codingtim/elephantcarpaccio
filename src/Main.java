f7f1fc171b2a26956481b60944c86da43579b47b
Tim Boeckstaens <boeckstaenstim@gmail.com>
2016-12-02 14:18:19 +0100

    UC1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("#\t$\tstate\tdiscount\ttax\tprice");
+        System.out.println("1\t100\t-\t-\t-\t100");
+
+    }
+}
