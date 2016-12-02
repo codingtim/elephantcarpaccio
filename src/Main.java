2cb35b1c9a16b186641052a69611b2f27397e496
Tim Boeckstaens <boeckstaenstim@gmail.com>
2016-12-02 14:29:01 +0100

    more output

--- src/Main.java
+++ src/Main.java
@@ -11,25 +11,26 @@ public class Main {
 
     private void run() {
 
-        double unitPrice = readDouble(0, Integer.MAX_VALUE, scanner);
+        double unitPrice = readDouble("Unit price", 0, Integer.MAX_VALUE, scanner);
         Order order = new Order(1, unitPrice);
 
         System.out.println("#\t$\tstate\tdiscount\ttax\tprice");
         System.out.println("1\t" + order.getUnitPrice() +"\t-\t-\t-\t" + order.total());
     }
 
-    private double readDouble(int minValue, int maxValue, Scanner scanner) {
+    private double readDouble(String field, int minValue, int maxValue, Scanner scanner) {
+        System.out.println("Enter the " + field);
         String line = scanner.nextLine();
         try {
             double number = Double.parseDouble(line);
             if(number < minValue || number > maxValue) {
                 System.out.println("Invalid input, expected number >= " + minValue + " and <= " + maxValue);
-                return readDouble(minValue, maxValue, scanner);
+                return readDouble("Unit price", minValue, maxValue, scanner);
             }
             return number;
         } catch (NumberFormatException e) {
             System.out.println("Invalid input, expected number >= " + minValue + " and <= " + maxValue);
-            return readDouble(minValue, maxValue, scanner);
+            return readDouble("Unit price", minValue, maxValue, scanner);
         }
     }
 }
