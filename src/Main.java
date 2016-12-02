b9a864c91c1d5c5a71a31f10f77f32dea4b1dd6d
Tim Boeckstaens <boeckstaenstim@gmail.com>
2016-12-02 14:23:15 +0100

    UC3

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,17 @@ public class Main {
 
     private void run() {
 
-        int unitPrice = readNumber(1, Integer.MAX_VALUE, scanner);
+        double unitPrice = readNumber(0, Integer.MAX_VALUE, scanner);
 
 
         System.out.println("#\t$\tstate\tdiscount\ttax\tprice");
         System.out.println("1\t" + unitPrice +"\t-\t-\t-\t100");
     }
 
-    private int readNumber(int minValue, int maxValue, Scanner scanner) {
+    private double readNumber(int minValue, int maxValue, Scanner scanner) {
         String line = scanner.nextLine();
         try {
-            int number = Integer.parseInt(line);
+            double number = Double.parseDouble(line);
             if(number < minValue || number > maxValue) {
                 System.out.println("Invalid input, expected number >= " + minValue + " and <= " + maxValue);
                 return readNumber(minValue, maxValue, scanner);
