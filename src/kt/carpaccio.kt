877e6d26ded76805b8c198b1418fcafdc71d02c1
Tim Boeckstaens <boeckstaenstim@gmail.com>
2016-12-03 13:19:42 +0100

    US 3

--- src/kt/carpaccio.kt
+++ src/kt/carpaccio.kt
@@ -6,16 +6,20 @@ import java.util.*
 fun main(args: Array<String>) {
     val reader = Reader(Scanner(System.`in`))
     val price = reader.readDouble("price", 1, Int.MAX_VALUE)
-    print(price)
+    print(Order(price))
 }
 
-fun print(price: Double) {
+fun print(order: Order) {
     System.out.println(StringBuilder()
             .append("Amount: ").append(1).append("\n")
-            .append("Price: ").append(price).append("\n")
+            .append("Price: ").append(order.price).append("\n")
             .append("State: ").append("-").append("\n")
             .append("Discount: ").append("-").append("\n")
             .append("TaxRate: ").append("-").append("\n")
-            .append("Total: ").append(100).append("\n")
+            .append("Total: ").append(order.total).append("\n")
     )
+}
+
+data class Order(val price: Double, val amount: Int = 1) {
+    val total = price * amount;
 }
