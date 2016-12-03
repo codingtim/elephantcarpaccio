4b84a3200658b8f8e01aad76bffa15baa4994859
Tim Boeckstaens <boeckstaenstim@gmail.com>
2016-12-03 13:23:54 +0100

    US 6

--- src/kt/carpaccio.kt
+++ src/kt/carpaccio.kt
@@ -1,5 +1,6 @@
 package kt
 
+import j.LivingState
 import j.Reader
 import java.util.*
 
@@ -7,14 +8,15 @@ fun main(args: Array<String>) {
     val reader = Reader(Scanner(System.`in`))
     val price = reader.readDouble("price", 1, Int.MAX_VALUE)
     val amount = reader.readInt("amount", 1, Int.MAX_VALUE)
-    print(Order(price, amount))
+    val state = reader.readEnum("state")
+    print(Order(price, amount), state)
 }
 
-fun print(order: Order) {
+fun print(order: Order, state: LivingState) {
     System.out.println(StringBuilder()
             .append("Amount: ").append(order.amount).append("\n")
             .append("Price: ").append(order.price).append("\n")
-            .append("State: ").append("-").append("\n")
+            .append("State: ").append(state.name).append("\n")
             .append("Discount: ").append("-").append("\n")
             .append("TaxRate: ").append("-").append("\n")
             .append("Total: ").append(order.total).append("\n")
