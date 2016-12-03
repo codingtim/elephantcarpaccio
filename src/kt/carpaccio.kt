ce854653e7a5c258dcb041b18490fcc55b2f287f
Tim Boeckstaens <boeckstaenstim@gmail.com>
2016-12-03 13:15:29 +0100

    US 2

--- src/kt/carpaccio.kt
+++ src/kt/carpaccio.kt
@@ -1,13 +1,18 @@
 package kt
 
+import j.Reader
+import java.util.*
+
 fun main(args: Array<String>) {
-    print()
+    val reader = Reader(Scanner(System.`in`))
+    val price = reader.readDouble("price", 1, Int.MAX_VALUE)
+    print(price)
 }
 
-fun print() {
+fun print(price: Double) {
     System.out.println(StringBuilder()
             .append("Amount: ").append(1).append("\n")
-            .append("Price: ").append(100).append("\n")
+            .append("Price: ").append(price).append("\n")
             .append("State: ").append("-").append("\n")
             .append("Discount: ").append("-").append("\n")
             .append("TaxRate: ").append("-").append("\n")
