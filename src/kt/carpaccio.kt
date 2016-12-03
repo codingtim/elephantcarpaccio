c0b4e7fd8adff0def5e9d138dec35da85945a222
Tim Boeckstaens <boeckstaenstim@gmail.com>
2016-12-03 13:29:03 +0100

    US 8 + 9

--- src/kt/carpaccio.kt
+++ src/kt/carpaccio.kt
@@ -9,18 +9,23 @@ fun main(args: Array<String>) {
     val price = reader.readDouble("price", 1, Int.MAX_VALUE)
     val amount = reader.readInt("amount", 1, Int.MAX_VALUE)
     val state = reader.readEnum("state")
-    print(Order(price, amount), state)
+    print(OrderWithTax(Order(price, amount), state))
 }
 
-fun print(order: Order, state: LivingState) {
+fun print(orderWithTax: OrderWithTax) {
     System.out.println(StringBuilder()
-            .append("Amount: ").append(order.amount).append("\n")
-            .append("Price: ").append(order.price).append("\n")
-            .append("State: ").append(state.name).append("\n")
+            .append("Amount: ").append(orderWithTax.order.amount).append("\n")
+            .append("Price: ").append(orderWithTax.order.price).append("\n")
+            .append("State: ").append(orderWithTax.state.name).append("\n")
             .append("Discount: ").append("-").append("\n")
-            .append("TaxRate: ").append(state.taxRate).append("\n")
-            .append("Total: ").append(order.total).append("\n")
+            .append("TaxRate: ").append(orderWithTax.state.taxRate).append("\n")
+            .append("Total: ").append(orderWithTax.order.total).append("\n")
+            .append("TotalWithTax: ").append(orderWithTax.totalWithTax).append("\n")
     )
+}
+
+data class OrderWithTax(val order: Order, val state: LivingState) {
+    val totalWithTax = order.total * (1 + (state.taxRate / 100))
 }
 
 data class Order(val price: Double, val amount: Int = 1) {
