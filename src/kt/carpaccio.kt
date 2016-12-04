3f66789d6a16479c259f542a33c2136e17688352
Tim Boeckstaens <boeckstaenstim@gmail.com>
2016-12-04 09:59:50 +0100

    kotlin discount

--- src/kt/carpaccio.kt
+++ src/kt/carpaccio.kt
@@ -9,7 +9,12 @@ fun main(args: Array<String>) {
     val price = reader.readDouble("price", 1, Int.MAX_VALUE)
     val amount = reader.readInt("amount", 1, Int.MAX_VALUE)
     val state = reader.readEnum("state")
-    print(Invoice(Order(price, amount), state))
+
+    val order = Order(price, amount)
+    val discount = calculateDiscount(order)
+    val invoice = Invoice(order, state, discount)
+
+    print(invoice)
 }
 
 fun print(invoice: Invoice) {
@@ -17,15 +22,40 @@ fun print(invoice: Invoice) {
             .append("Amount: ").append(invoice.order.amount).append("\n")
             .append("Price: ").append(invoice.order.price).append("\n")
             .append("State: ").append(invoice.state.name).append("\n")
-            .append("Discount: ").append("-").append("\n")
+            .append("Discount: ").append(invoice.totalDiscount).append("\n")
             .append("TaxRate: ").append(invoice.state.taxRate).append("%\n")
-            .append("Total: ").append(invoice.order.total).append("\n")
+            .append("Total: ").append(invoice.total).append("\n")
             .append("TotalWithTax: ").append(invoice.totalWithTax).append("\n")
     )
 }
 
-data class Invoice(val order: Order, val state: LivingState) {
-    val totalWithTax = order.total * (1 + (state.taxRate / 100))
+fun calculateDiscount(order: Order): Discount {
+    return when {
+        order.total in 50000..Integer.MAX_VALUE -> PercentageDiscount(0.15)
+        order.total in 10000..49999 -> PercentageDiscount(0.10)
+        order.total in 7000..9999 -> PercentageDiscount(0.07)
+        order.total in 5000..6999 -> PercentageDiscount(0.05)
+        order.total in 1000..4999 -> PercentageDiscount(0.03)
+        else -> NoDiscount()
+    }
+}
+
+interface Discount {
+    fun getDiscount(total: Double): Double
+}
+
+data class NoDiscount(val required: Unit = Unit) : Discount {
+    override fun getDiscount(total: Double): Double = 0.0
+}
+
+data class PercentageDiscount(val percentage: Double) : Discount {
+    override fun getDiscount(total: Double): Double = total * percentage
+}
+
+data class Invoice(val order: Order, val state: LivingState, val discount: Discount) {
+    val totalDiscount = discount.getDiscount(order.total)
+    val totalWithTax = (order.total - totalDiscount) * (1 + (state.taxRate / 100))
+    val total = order.total - totalDiscount
 }
 
 data class Order(val price: Double, val amount: Int = 1) {
